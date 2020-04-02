5773dcc9481033ef48271230c4f3f50a8cb51bd8
Roberto Ribeiro <robertov82008@gmail.com>
2020-04-01 21:04:07 -0300

    10 - Utilizando Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="" />
 
-      <form>
-        <input
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name="email"
           type="email"
           placeholder="Seu e-mail"
           autoComplete="new-password"
         />
-        <input
+        <Input
+          name="password"
           type="password"
           placeholder="Sua senha secreta"
           autoComplete="new-password"
@@ -22,7 +29,7 @@ export default function SignIn() {
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="" />
 
-      <form>
-        <input
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name="name"
           type="text"
           placeholder="Nome Completo"
           autoComplete="new-password"
         />
 
-        <input
+        <Input
+          name="email"
           type="email"
           placeholder="Seu e-mail"
           autoComplete="new-password"
         />
-        <input
+        <Input
+          name="password"
           type="password"
           placeholder="Sua senha secreta"
           autoComplete="new-password"
@@ -28,7 +36,7 @@ export default function SignUp() {
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
