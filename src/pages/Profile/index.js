c59e08574d63febe74ce443439696435f9a13c02
Roberto Ribeiro <robertov82008@gmail.com>
2020-04-05 12:57:15 -0300

    25 - Foto de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import AvatarInput from '~/pages/Profile/AvatarInput';
+
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from '~/pages/Profile/styles';
@@ -17,6 +19,8 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu Endereço de E-mail" />
 
